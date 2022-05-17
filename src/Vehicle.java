05bdd6b9809afb021bd3495dd51e4c0cb7ddc9b3
User <some_email@example.com>
2022-05-17 21:20:40 +0300

    Первый коммит

---
+++ src/Vehicle.java
@@ -0,0 +1,35 @@
+public class Vehicle {
+    public Vehicle(){
+        model = "BMW";
+        maxspeed = 400;
+        isDieselFuel = false;
+        fuel = 0;
+    }
+    String model;
+    int maxspeed;
+    boolean isDieselFuel;
+    int fuel;
+
+
+    public String getModel(){
+        return model;
+    }
+    void drive() {
+        fuel -= 50;
+        System.out.println("Заводит....");
+    }
+    void stop(){
+        fuel = fuel;
+        System.out.println("*Едет*");
+    }
+    void refuel(){
+        fuel = 100;
+        System.out.println("*Заправляется*");
+    }
+
+
+    public static void main(String[] args){
+        Vehicle vehicle = new Vehicle();
+
+    }
+}
